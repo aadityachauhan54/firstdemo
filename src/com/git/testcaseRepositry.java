399e314f72d51ab5991e4319305073a6ad7093fa
aadityachauhan54 <aadityachauhan54@gmail.com>
2024-02-06 22:54:36 +0530

    string program

---
+++ src/com/git/testcaseRepositry.java
@@ -0,0 +1,14 @@
+package com.git;
+
+public class testcaseRepositry {
+
+	public static void main(String[] args) {
+	String str = "i am automation tester";
+	String ss[] = str.split(" ");
+	for(int i=ss.length-1;i>=0;i--) {
+		System.out.print(ss[i]+" ");
+	}
+
+	}
+
+}
